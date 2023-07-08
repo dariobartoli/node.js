1eef7226217717844b6f7e31c30b56548826f9dc
dariobartoli <113705473+dariobartoli@users.noreply.github.com>
2023-07-07 22:52:34 -0300

    añadido ejercicio de rubik

---
+++ rubik.js
@@ -0,0 +1,207 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            probando: "hola",
+            cubo: []
+        }
+    },
+    created(){
+        this.cubo = {
+            top: ["negro","negro","negro","negro","negro","negro","negro","negro","negro"],
+            lateralIzq: ["azul","azul","azul","azul","azul","azul","azul","azul","azul"],
+            frontal: ["naranja","naranja","naranja","naranja","naranja","naranja","naranja","naranja","naranja"],
+            lateralDer: ["blanco","blanco","blanco","blanco","blanco","blanco","blanco","blanco","blanco"],
+            detras: ["verde","verde","verde","verde","verde","verde","verde","verde","verde"],
+            bottom: ["rojo","rojo","rojo","rojo","rojo","rojo","rojo","rojo","rojo"]
+        }
+    },
+    methods:{
+        abajoDer(){
+            let newLateralIzq = this.cubo.lateralIzq.slice(0,6).concat(this.cubo.detras.slice(6,9))
+            let newFrontal = this.cubo.frontal.slice(0,6).concat(this.cubo.lateralIzq.slice(6,9))
+            let newLateralDer = this.cubo.lateralDer.slice(0,6).concat(this.cubo.frontal.slice(6,9))
+            let newDetras = this.cubo.detras.slice(0,6).concat(this.cubo.lateralDer.slice(6,9))
+            let elementosMovidos = [
+                this.cubo.bottom[0],this.cubo.bottom[1],this.cubo.bottom[2],this.cubo.bottom[3],this.cubo.bottom[4],this.cubo.bottom[5],this.cubo.bottom[6],this.cubo.bottom[7],this.cubo.bottom[8]
+            ]
+
+            this.cubo.lateralIzq = newLateralIzq
+            this.cubo.frontal = newFrontal
+            this.cubo.lateralDer = newLateralDer
+            this.cubo.detras = newDetras
+            this.cubo.bottom = [
+                this.cubo.bottom[0],this.cubo.bottom[1],this.cubo.bottom[2],this.cubo.bottom[3],this.cubo.bottom[4],this.cubo.bottom[5],this.cubo.bottom[6],this.cubo.bottom[7],this.cubo.bottom[8]] = [elementosMovidos[6], elementosMovidos[3],elementosMovidos[0],elementosMovidos[7],elementosMovidos[4],elementosMovidos[1],elementosMovidos[8],elementosMovidos[5],elementosMovidos[2]
+            ]
+
+            console.log(this.cubo.frontal.slice(1,7).concat(this.cubo.lateralIzq.slice(6,10)));
+        },
+        abajoIzq(){
+            let newLateralIzq = this.cubo.lateralIzq.slice(0,6).concat(this.cubo.frontal.slice(6,9))
+            let newFrontal = this.cubo.frontal.slice(0,6).concat(this.cubo.lateralDer.slice(6,9))
+            let newLateralDer = this.cubo.lateralDer.slice(0,6).concat(this.cubo.detras.slice(6,9))
+            let newDetras = this.cubo.detras.slice(0,6).concat(this.cubo.lateralIzq.slice(6,9))
+            let elementosMovidos = [
+                this.cubo.bottom[0],this.cubo.bottom[1],this.cubo.bottom[2],this.cubo.bottom[3],this.cubo.bottom[4],this.cubo.bottom[5],this.cubo.bottom[6],this.cubo.bottom[7],this.cubo.bottom[8]
+            ]
+
+            this.cubo.lateralIzq = newLateralIzq
+            this.cubo.frontal = newFrontal
+            this.cubo.lateralDer = newLateralDer
+            this.cubo.detras = newDetras
+            this.cubo.bottom = [
+                this.cubo.bottom[0],this.cubo.bottom[1],this.cubo.bottom[2],this.cubo.bottom[3],this.cubo.bottom[4],this.cubo.bottom[5],this.cubo.bottom[6],this.cubo.bottom[7],this.cubo.bottom[8]] = [elementosMovidos[2], elementosMovidos[5],elementosMovidos[8],elementosMovidos[1],elementosMovidos[4],elementosMovidos[7],elementosMovidos[0],elementosMovidos[3],elementosMovidos[6]
+            ]
+        },
+        arribaIzq(){
+            let newLateralIzq = this.cubo.frontal.slice(0,3).concat(this.cubo.lateralIzq.slice(3,9))
+            let newFrontal = this.cubo.lateralDer.slice(0,3).concat(this.cubo.frontal.slice(3,9))
+            let newLateralDer = this.cubo.detras.slice(0,3).concat(this.cubo.lateralDer.slice(3,9))
+            let newDetras = this.cubo.lateralIzq.slice(0,3).concat(this.cubo.detras.slice(3,9))
+            let elementosMovidos = [
+                this.cubo.top[0],this.cubo.top[1],this.cubo.top[2],this.cubo.top[3],this.cubo.top[4],this.cubo.top[5],this.cubo.top[6],this.cubo.top[7],this.cubo.top[8]
+            ]
+
+            this.cubo.lateralIzq = newLateralIzq
+            this.cubo.frontal = newFrontal
+            this.cubo.lateralDer = newLateralDer
+            this.cubo.detras = newDetras
+            this.cubo.top = [
+                this.cubo.top[0],this.cubo.top[1],this.cubo.top[2],this.cubo.top[3],this.cubo.top[4],this.cubo.top[5],this.cubo.top[6],this.cubo.top[7],this.cubo.top[8]] = [elementosMovidos[6], elementosMovidos[3],elementosMovidos[0],elementosMovidos[7],elementosMovidos[4],elementosMovidos[1],elementosMovidos[8],elementosMovidos[5],elementosMovidos[2]
+            ]
+        },
+        arribaDer(){
+            let newLateralIzq = this.cubo.detras.slice(0,3).concat(this.cubo.lateralIzq.slice(3,9))
+            let newFrontal = this.cubo.lateralIzq.slice(0,3).concat(this.cubo.frontal.slice(3,9))
+            let newLateralDer = this.cubo.frontal.slice(0,3).concat(this.cubo.lateralDer.slice(3,9))
+            let newDetras = this.cubo.lateralDer.slice(0,3).concat(this.cubo.detras.slice(3,9))
+            let elementosMovidos = [
+                this.cubo.top[0],this.cubo.top[1],this.cubo.top[2],this.cubo.top[3],this.cubo.top[4],this.cubo.top[5],this.cubo.top[6],this.cubo.top[7],this.cubo.top[8]
+            ]
+
+            this.cubo.lateralIzq = newLateralIzq
+            this.cubo.frontal = newFrontal
+            this.cubo.lateralDer = newLateralDer
+            this.cubo.detras = newDetras
+            this.cubo.top= [
+                this.cubo.top[0],this.cubo.top[1],this.cubo.top[2],this.cubo.top[3],this.cubo.top[4],this.cubo.top[5],this.cubo.top[6],this.cubo.top[7],this.cubo.top[8]] = [elementosMovidos[2], elementosMovidos[5],elementosMovidos[8],elementosMovidos[1],elementosMovidos[4],elementosMovidos[7],elementosMovidos[0],elementosMovidos[3],elementosMovidos[6]
+            ]
+        },
+        lateralDerechoAbajo(){
+            let elementosNaranja = [this.cubo.frontal[2],this.cubo.frontal[5],this.cubo.frontal[8]]
+            let elementosRojo = [this.cubo.bottom[2],this.cubo.bottom[5],this.cubo.bottom[8]]
+            let elementosVerde = [this.cubo.detras[2],this.cubo.detras[5],this.cubo.detras[8]]
+            let elementosNegro = [this.cubo.top[2],this.cubo.top[5],this.cubo.top[8]]
+            let elementosMovidos = [
+                this.cubo.lateralDer[0],this.cubo.lateralDer[1],this.cubo.lateralDer[2],this.cubo.lateralDer[3],this.cubo.lateralDer[4],this.cubo.lateralDer[5],this.cubo.lateralDer[6],this.cubo.lateralDer[7],this.cubo.lateralDer[8]
+            ]
+        
+            this.cubo.frontal = [
+                this.cubo.frontal[0],this.cubo.frontal[1],this.cubo.frontal[2],this.cubo.frontal[3],this.cubo.frontal[4],this.cubo.frontal[5],this.cubo.frontal[6],this.cubo.frontal[7],this.cubo.frontal[8]] = [this.cubo.frontal[0],this.cubo.frontal[1], elementosNegro[0] ,this.cubo.frontal[3],this.cubo.frontal[4],elementosNegro[1],this.cubo.frontal[6],this.cubo.frontal[7],elementosNegro[2]
+            ]
+
+            this.cubo.bottom = [
+                this.cubo.bottom[0],this.cubo.bottom[1],this.cubo.bottom[2],this.cubo.bottom[3],this.cubo.bottom[4],this.cubo.bottom[5],this.cubo.bottom[6],this.cubo.bottom[7],this.cubo.bottom[8]] = [this.cubo.bottom[0],this.cubo.bottom[1],elementosNaranja[0],this.cubo.bottom[3],this.cubo.bottom[4],elementosNaranja[1],this.cubo.bottom[6],this.cubo.bottom[7],elementosNaranja[2]
+            ]
+
+            this.cubo.detras = [
+                this.cubo.detras[0],this.cubo.detras[1],this.cubo.detras[2],this.cubo.detras[3],this.cubo.detras[4],this.cubo.detras[5],this.cubo.detras[6],this.cubo.detras[7],this.cubo.detras[8]] = [this.cubo.detras[0],this.cubo.detras[1],elementosRojo[0],this.cubo.detras[3],this.cubo.detras[4],elementosRojo[1],this.cubo.detras[6],this.cubo.detras[7],elementosRojo[2]
+            ]
+
+            this.cubo.top = [
+                this.cubo.top[0],this.cubo.top[1],this.cubo.top[2],this.cubo.top[3],this.cubo.top[4],this.cubo.top[5],this.cubo.top[6],this.cubo.top[7],this.cubo.top[8]] = [this.cubo.top[0],this.cubo.top[1],elementosVerde[0],this.cubo.top[3],this.cubo.top[4],elementosVerde[1],this.cubo.top[6],this.cubo.top[7],elementosVerde[2]
+            ]
+
+            this.cubo.lateralDer= [
+                this.cubo.lateralDer[0],this.cubo.lateralDer[1],this.cubo.lateralDer[2],this.cubo.lateralDer[3],this.cubo.lateralDer[4],this.cubo.lateralDer[5],this.cubo.lateralDer[6],this.cubo.lateralDer[7],this.cubo.lateralDer[8]] = [elementosMovidos[2], elementosMovidos[5],elementosMovidos[8],elementosMovidos[1],elementosMovidos[4],elementosMovidos[7],elementosMovidos[0],elementosMovidos[3],elementosMovidos[6]
+            ]
+        },
+        lateralDerechoArriba(){
+            let elementosNaranja = [this.cubo.frontal[2],this.cubo.frontal[5],this.cubo.frontal[8]]
+            let elementosRojo = [this.cubo.bottom[2],this.cubo.bottom[5],this.cubo.bottom[8]]
+            let elementosVerde = [this.cubo.detras[2],this.cubo.detras[5],this.cubo.detras[8]]
+            let elementosNegro = [this.cubo.top[2],this.cubo.top[5],this.cubo.top[8]]
+            let elementosMovidos = [
+                this.cubo.lateralDer[0],this.cubo.lateralDer[1],this.cubo.lateralDer[2],this.cubo.lateralDer[3],this.cubo.lateralDer[4],this.cubo.lateralDer[5],this.cubo.lateralDer[6],this.cubo.lateralDer[7],this.cubo.lateralDer[8]
+            ]
+        
+            this.cubo.frontal = [
+                this.cubo.frontal[0],this.cubo.frontal[1],this.cubo.frontal[2],this.cubo.frontal[3],this.cubo.frontal[4],this.cubo.frontal[5],this.cubo.frontal[6],this.cubo.frontal[7],this.cubo.frontal[8]] = [this.cubo.frontal[0],this.cubo.frontal[1], elementosRojo[0] ,this.cubo.frontal[3],this.cubo.frontal[4],elementosRojo[1],this.cubo.frontal[6],this.cubo.frontal[7],elementosRojo[2]
+            ]
+
+            this.cubo.bottom = [
+                this.cubo.bottom[0],this.cubo.bottom[1],this.cubo.bottom[2],this.cubo.bottom[3],this.cubo.bottom[4],this.cubo.bottom[5],this.cubo.bottom[6],this.cubo.bottom[7],this.cubo.bottom[8]] = [this.cubo.bottom[0],this.cubo.bottom[1],elementosVerde[0],this.cubo.bottom[3],this.cubo.bottom[4],elementosVerde[1],this.cubo.bottom[6],this.cubo.bottom[7],elementosVerde[2]
+            ]
+
+            this.cubo.detras = [
+                this.cubo.detras[0],this.cubo.detras[1],this.cubo.detras[2],this.cubo.detras[3],this.cubo.detras[4],this.cubo.detras[5],this.cubo.detras[6],this.cubo.detras[7],this.cubo.detras[8]] = [this.cubo.detras[0],this.cubo.detras[1],elementosNegro[0],this.cubo.detras[3],this.cubo.detras[4],elementosNegro[1],this.cubo.detras[6],this.cubo.detras[7],elementosNegro[2]
+            ]
+
+            this.cubo.top = [
+                this.cubo.top[0],this.cubo.top[1],this.cubo.top[2],this.cubo.top[3],this.cubo.top[4],this.cubo.top[5],this.cubo.top[6],this.cubo.top[7],this.cubo.top[8]] = [this.cubo.top[0],this.cubo.top[1],elementosNaranja[0],this.cubo.top[3],this.cubo.top[4],elementosNaranja[1],this.cubo.top[6],this.cubo.top[7],elementosNaranja[2]
+            ]
+
+            this.cubo.lateralDer = [
+                this.cubo.lateralDer[0],this.cubo.lateralDer[1],this.cubo.lateralDer[2],this.cubo.lateralDer[3],this.cubo.lateralDer[4],this.cubo.lateralDer[5],this.cubo.lateralDer[6],this.cubo.lateralDer[7],this.cubo.lateralDer[8]] = [elementosMovidos[6], elementosMovidos[3],elementosMovidos[0],elementosMovidos[7],elementosMovidos[4],elementosMovidos[1],elementosMovidos[8],elementosMovidos[5],elementosMovidos[2]
+            ]
+        },
+        lateralIzquierdoAbajo(){
+            let elementosNaranja = [this.cubo.frontal[0],this.cubo.frontal[3],this.cubo.frontal[6]]
+            let elementosRojo = [this.cubo.bottom[0],this.cubo.bottom[3],this.cubo.bottom[6]]
+            let elementosVerde = [this.cubo.detras[0],this.cubo.detras[3],this.cubo.detras[6]]
+            let elementosNegro = [this.cubo.top[0],this.cubo.top[3],this.cubo.top[6]]
+            let elementosMovidos = [
+                this.cubo.lateralIzq[0],this.cubo.lateralIzq[1],this.cubo.lateralIzq[2],this.cubo.lateralIzq[3],this.cubo.lateralIzq[4],this.cubo.lateralIzq[5],this.cubo.lateralIzq[6],this.cubo.lateralIzq[7],this.cubo.lateralIzq[8]
+            ]
+        
+            this.cubo.frontal = [
+                this.cubo.frontal[0],this.cubo.frontal[1],this.cubo.frontal[2],this.cubo.frontal[3],this.cubo.frontal[4],this.cubo.frontal[5],this.cubo.frontal[6],this.cubo.frontal[7],this.cubo.frontal[8]] = [elementosNegro[0],this.cubo.frontal[1],this.cubo.frontal[2],elementosNegro[1],this.cubo.frontal[4],this.cubo.frontal[5],elementosNegro[2],this.cubo.frontal[7],this.cubo.frontal[8]
+            ]
+
+            this.cubo.bottom = [
+                this.cubo.bottom[0],this.cubo.bottom[1],this.cubo.bottom[2],this.cubo.bottom[3],this.cubo.bottom[4],this.cubo.bottom[5],this.cubo.bottom[6],this.cubo.bottom[7],this.cubo.bottom[8]] = [elementosNaranja[0],this.cubo.bottom[1],this.cubo.bottom[2],elementosNaranja[1],this.cubo.bottom[4],this.cubo.bottom[5],elementosNaranja[1],this.cubo.bottom[7],this.cubo.bottom[8]
+            ]
+
+            this.cubo.detras = [
+                this.cubo.detras[0],this.cubo.detras[1],this.cubo.detras[2],this.cubo.detras[3],this.cubo.detras[4],this.cubo.detras[5],this.cubo.detras[6],this.cubo.detras[7],this.cubo.detras[8]] = [elementosRojo[0],this.cubo.detras[1],this.cubo.detras[2],elementosRojo[1],this.cubo.detras[4],this.cubo.detras[5],elementosRojo[2],this.cubo.detras[7],this.cubo.detras[8]
+            ]
+
+            this.cubo.top = [
+                this.cubo.top[0],this.cubo.top[1],this.cubo.top[2],this.cubo.top[3],this.cubo.top[4],this.cubo.top[5],this.cubo.top[6],this.cubo.top[7],this.cubo.top[8]] = [elementosVerde[0],this.cubo.top[1],this.cubo.top[2],elementosVerde[1],this.cubo.top[4],this.cubo.top[5],elementosVerde[1],this.cubo.top[7],this.cubo.top[8]
+            ]
+
+            this.cubo.lateralIzq= [
+                this.cubo.lateralIzq[0],this.cubo.lateralIzq[1],this.cubo.lateralIzq[2],this.cubo.lateralIzq[3],this.cubo.lateralIzq[4],this.cubo.lateralIzq[5],this.cubo.lateralIzq[6],this.cubo.lateralIzq[7],this.cubo.lateralIzq[8]] = [elementosMovidos[6], elementosMovidos[3],elementosMovidos[0],elementosMovidos[7],elementosMovidos[4],elementosMovidos[1],elementosMovidos[8],elementosMovidos[5],elementosMovidos[2]
+            ]
+        },
+        lateralIzquierdoArriba(){
+            let elementosNaranja = [this.cubo.frontal[0],this.cubo.frontal[3],this.cubo.frontal[6]]
+            let elementosRojo = [this.cubo.bottom[0],this.cubo.bottom[3],this.cubo.bottom[6]]
+            let elementosVerde = [this.cubo.detras[0],this.cubo.detras[3],this.cubo.detras[6]]
+            let elementosNegro = [this.cubo.top[0],this.cubo.top[3],this.cubo.top[6]]
+            let elementosMovidos = [
+                this.cubo.lateralIzq[0],this.cubo.lateralIzq[1],this.cubo.lateralIzq[2],this.cubo.lateralIzq[3],this.cubo.lateralIzq[4],this.cubo.lateralIzq[5],this.cubo.lateralIzq[6],this.cubo.lateralIzq[7],this.cubo.lateralIzq[8]
+            ]
+        
+            this.cubo.frontal = [
+                this.cubo.frontal[0],this.cubo.frontal[1],this.cubo.frontal[2],this.cubo.frontal[3],this.cubo.frontal[4],this.cubo.frontal[5],this.cubo.frontal[6],this.cubo.frontal[7],this.cubo.frontal[8]] = [elementosRojo[0],this.cubo.frontal[1],this.cubo.frontal[2],elementosRojo[1],this.cubo.frontal[4],this.cubo.frontal[5],elementosRojo[2],this.cubo.frontal[7],this.cubo.frontal[8]
+            ]
+
+            this.cubo.bottom = [
+                this.cubo.bottom[0],this.cubo.bottom[1],this.cubo.bottom[2],this.cubo.bottom[3],this.cubo.bottom[4],this.cubo.bottom[5],this.cubo.bottom[6],this.cubo.bottom[7],this.cubo.bottom[8]] = [elementosVerde[0],this.cubo.bottom[1],this.cubo.bottom[2],elementosVerde[1],this.cubo.bottom[4],this.cubo.bottom[5],elementosVerde[1],this.cubo.bottom[7],this.cubo.bottom[8]
+            ]
+
+            this.cubo.detras = [
+                this.cubo.detras[0],this.cubo.detras[1],this.cubo.detras[2],this.cubo.detras[3],this.cubo.detras[4],this.cubo.detras[5],this.cubo.detras[6],this.cubo.detras[7],this.cubo.detras[8]] = [elementosNegro[0],this.cubo.detras[1],this.cubo.detras[2],elementosNegro[1],this.cubo.detras[4],this.cubo.detras[5],elementosNegro[2],this.cubo.detras[7],this.cubo.detras[8]
+            ]
+
+            this.cubo.top = [
+                this.cubo.top[0],this.cubo.top[1],this.cubo.top[2],this.cubo.top[3],this.cubo.top[4],this.cubo.top[5],this.cubo.top[6],this.cubo.top[7],this.cubo.top[8]] = [elementosNaranja[0],this.cubo.top[1],this.cubo.top[2],elementosNaranja[1],this.cubo.top[4],this.cubo.top[5],elementosNaranja[1],this.cubo.top[7],this.cubo.top[8]
+            ]
+
+            this.cubo.lateralIzq= [
+                this.cubo.lateralIzq[0],this.cubo.lateralIzq[1],this.cubo.lateralIzq[2],this.cubo.lateralIzq[3],this.cubo.lateralIzq[4],this.cubo.lateralIzq[5],this.cubo.lateralIzq[6],this.cubo.lateralIzq[7],this.cubo.lateralIzq[8]] = [elementosMovidos[2], elementosMovidos[5],elementosMovidos[8],elementosMovidos[1],elementosMovidos[4],elementosMovidos[7],elementosMovidos[0],elementosMovidos[3],elementosMovidos[6]
+            ]
+        }
+    }
+})
+app.mount("#app")
---
+++ rubik2.js
@@ -0,0 +1,111 @@
+let cubo = {
+    top: ["negro","negro","negro","negro","negro","negro","negro","negro","negro"],
+    lateralIzq: ["azul","azul","azul","azul","azul","azul","azul","azul","azul"],
+    frontal: ["naranja","naranja","naranja","naranja","naranja","naranja","naranja","naranja","naranja"],
+    lateralDer: ["blanco","blanco","blanco","blanco","blanco","blanco","blanco","blanco","blanco"],
+    detras: ["verde","verde","verde","verde","verde","verde","verde","verde","verde"],
+    bottom: ["rojo","rojo","rojo","rojo","rojo","rojo","rojo","rojo","rojo"],
+    bot: [1,2,3,4,5,6,7,8,9],
+}
+
+
+function abajoDer(){
+    let newLateralIzq = cubo.lateralIzq.slice(0,6).concat(cubo.detras.slice(6,9))
+    let newFrontal = cubo.frontal.slice(0,6).concat(cubo.lateralIzq.slice(6,9))
+    let newLateralDer = cubo.lateralDer.slice(0,6).concat(cubo.frontal.slice(6,9))
+    let newDetras = cubo.detras.slice(0,6).concat(cubo.lateralDer.slice(6,9))
+    let elementosMovidos = [
+        cubo.bottom[0],cubo.bottom[1],cubo.bottom[2],cubo.bottom[3],cubo.bottom[4],cubo.bottom[5],cubo.bottom[6],cubo.bottom[7],cubo.bottom[8]
+    ]
+
+    cubo.lateralIzq = newLateralIzq
+    cubo.frontal = newFrontal
+    cubo.lateralDer = newLateralDer
+    cubo.detras = newDetras
+    cubo.bottom = [
+        cubo.bottom[0],cubo.bottom[1],cubo.bottom[2],cubo.bottom[3],cubo.bottom[4],cubo.bottom[5],cubo.bottom[6],cubo.bottom[7],cubo.bottom[8]] = [elementosMovidos[6], elementosMovidos[3],elementosMovidos[0],elementosMovidos[7],elementosMovidos[4],elementosMovidos[1],elementosMovidos[8],elementosMovidos[5],elementosMovidos[2]
+    ]
+}
+function abajoIzq(){
+    let newLateralIzq = cubo.lateralIzq.slice(0,6).concat(cubo.frontal.slice(6,9))
+    let newFrontal = cubo.frontal.slice(0,6).concat(cubo.lateralDer.slice(6,9))
+    let newLateralDer = cubo.lateralDer.slice(0,6).concat(cubo.detras.slice(6,9))
+    let newDetras = cubo.detras.slice(0,6).concat(cubo.lateralIzq.slice(6,9))
+    let elementosMovidos = [
+        cubo.bottom[0],cubo.bottom[1],cubo.bottom[2],cubo.bottom[3],cubo.bottom[4],cubo.bottom[5],cubo.bottom[6],cubo.bottom[7],cubo.bottom[8]
+    ]
+
+    cubo.lateralIzq = newLateralIzq
+    cubo.frontal = newFrontal
+    cubo.lateralDer = newLateralDer
+    cubo.detras = newDetras
+    cubo.bottom = [
+        cubo.bottom[0],cubo.bottom[1],cubo.bottom[2],cubo.bottom[3],cubo.bottom[4],cubo.bottom[5],cubo.bottom[6],cubo.bottom[7],cubo.bottom[8]] = [elementosMovidos[2], elementosMovidos[5],elementosMovidos[8],elementosMovidos[1],elementosMovidos[4],elementosMovidos[7],elementosMovidos[0],elementosMovidos[3],elementosMovidos[6]
+    ]
+}
+
+function arribaIzq(){
+    let newLateralIzq = cubo.frontal.slice(0,3).concat(cubo.lateralIzq.slice(3,9))
+    let newFrontal = cubo.lateralDer.slice(0,3).concat(cubo.frontal.slice(3,9))
+    let newLateralDer = cubo.detras.slice(0,3).concat(cubo.lateralDer.slice(3,9))
+    let newDetras = cubo.lateralIzq.slice(0,3).concat(cubo.detras.slice(3,9))
+    let elementosMovidos = [
+        cubo.top[0],cubo.top[1],cubo.top[2],cubo.top[3],cubo.top[4],cubo.top[5],cubo.top[6],cubo.top[7],cubo.top[8]
+    ]
+
+    cubo.lateralIzq = newLateralIzq
+    cubo.frontal = newFrontal
+    cubo.lateralDer = newLateralDer
+    cubo.detras = newDetras
+
+    cubo.top = [
+        cubo.top[0],cubo.top[1],cubo.top[2],cubo.top[3],cubo.top[4],cubo.top[5],cubo.top[6],cubo.top[7],cubo.top[8]] = [elementosMovidos[6], elementosMovidos[3],elementosMovidos[0],elementosMovidos[7],elementosMovidos[4],elementosMovidos[1],elementosMovidos[8],elementosMovidos[5],elementosMovidos[2]
+    ]
+
+}
+
+function arribaDer(){
+    let newLateralIzq = cubo.detras.slice(0,3).concat(cubo.lateralIzq.slice(3,9))
+    let newFrontal = cubo.lateralIzq.slice(0,3).concat(cubo.frontal.slice(3,9))
+    let newLateralDer = cubo.frontal.slice(0,3).concat(cubo.lateralDer.slice(3,9))
+    let newDetras = cubo.lateralDer.slice(0,3).concat(cubo.detras.slice(3,9))
+    let elementosMovidos = [
+        cubo.top[0],cubo.top[1],cubo.top[2],cubo.top[3],cubo.top[4],cubo.top[5],cubo.top[6],cubo.top[7],cubo.top[8]
+    ]
+
+    cubo.lateralIzq = newLateralIzq
+    cubo.frontal = newFrontal
+    cubo.lateralDer = newLateralDer
+    cubo.detras = newDetras
+    cubo.top= [
+        cubo.top[0],cubo.top[1],cubo.top[2],cubo.top[3],cubo.top[4],cubo.top[5],cubo.top[6],cubo.top[7],cubo.top[8]] = [elementosMovidos[2], elementosMovidos[5],elementosMovidos[8],elementosMovidos[1],elementosMovidos[4],elementosMovidos[7],elementosMovidos[0],elementosMovidos[3],elementosMovidos[6]
+    ]
+}
+
+function lateralDerecho(){
+    let elementosNaranja = [cubo.frontal[2],cubo.frontal[5],cubo.frontal[8]]
+    let elementosRojo = [cubo.bottom[2],cubo.bottom[5],cubo.bottom[8]]
+    let elementosVerde = [cubo.detras[2],cubo.detras[5],cubo.detras[8]]
+    let elementosNegro = [cubo.top[2],cubo.top[5],cubo.top[8]]
+
+    cubo.frontal =[
+        ...cubo.frontal, cubo.frontal[2] = elementosNegro[0], cubo.frontal[5] = elementosNegro[1], cubo.frontal[8] = elementosNegro[2]
+    ]
+    cubo.bottom =[
+        ...cubo.bottom, cubo.bottom[2] = elementosNaranja[0], cubo.bottom[5] = elementosNaranja[1], cubo.bottom[8] = elementosNaranja[2]
+    ]
+    cubo.detras =[
+        ...cubo.detras, cubo.detras[2] = elementosRojo[0], cubo.detras[5] = elementosRojo[1], cubo.detras[8] = elementosRojo[2]
+    ]
+    cubo.top =[
+        ...cubo.top, cubo.top[2] = elementosVerde[0], cubo.top[5] = elementosVerde[1], cubo.top[8] = elementosVerde[2]
+    ]
+}
+
+lateralDerecho()
+
+
+console.log(cubo);
+
+  
+
